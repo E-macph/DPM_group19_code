30120e30196a095e99ca3eabb6142c6d2b8fbf12
EthanVal <valente.ethan21@gmail.com>
2025-09-22 16:40:21 -0400

    removed try statement

--- lab_2/lab2-starter-code/project/collect_color_sensor_data.py
+++ lab_2/lab2-starter-code/project/collect_color_sensor_data.py
@@ -18,42 +18,37 @@ wait_ready_sensors(True) # Input True to see what the robot is trying to initial
 
 def collect_color_sensor_data():
     "Collect color sensor data."
-    try:
-        output_file = open(COLOR_SENSOR_DATA_FILE, "a")
-        while not Touch_sensor.is_pressed():
-            pass  # do nothing while waiting for first button press #
+    output_file = open(COLOR_SENSOR_DATA_FILE, "a")
+    while not Touch_sensor.is_pressed():
+        pass  # do nothing while waiting for first button press #
 
-        print("Touch sensor pressed")
-        sleep(1)
-        print("Starting to collect Color distance samples")
+    print("Touch sensor pressed")
+    sleep(1)
+    print("Starting to collect Color distance samples")
 
-        while color_data < 10:
-            print("in color data list")
-            if Touch_sensor.is_pressed():
-                print("1")
-                rgb_list = Color_Sensor.get_rgb()  # Float value in centimeters 0, capped to 255 cm
-                print("2")
-                sleep(0.5)
-                print(rgb_list)
-                print("3")
+    while color_data < 10:
+        print("in color data list")
+        if Touch_sensor.is_pressed():
+            print("1")
+            rgb_list = Color_Sensor.get_rgb()  # Float value in centimeters 0, capped to 255 cm
+            print("2")
+            sleep(0.5)
+            print(rgb_list)
+            print("3")
 
-                if rgb_list is not None:  # If None is given, then data collection failed that time
-                    output_file.write(f"{rgb_list}\n")
-                    color_data += 1
-                    rgb_list = None
+            if rgb_list is not None:  # If None is given, then data collection failed that time
+                output_file.write(f"{rgb_list}\n")
+                color_data += 1
+                rgb_list = None
 
-                sleep(DELAY_SEC)
-
+            sleep(DELAY_SEC)
 
 
-    except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
-        print("Exception caught" + BaseException)
-        pass
-    finally:
-        print("Done collecting color distance samples")
-        output_file.close()
-        reset_brick()  # Turn off everything on the brick's hardware, and reset it
-        exit()
+
+    print("Done collecting color distance samples")
+    output_file.close()
+    reset_brick()  # Turn off everything on the brick's hardware, and reset it
+    exit()
 
 if __name__ == "__main__":
     collect_color_sensor_data()
