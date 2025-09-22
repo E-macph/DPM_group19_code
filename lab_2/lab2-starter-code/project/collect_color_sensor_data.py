c0671804925e5dd760a7b215b174b4746df200f7
EthanVal <valente.ethan21@gmail.com>
2025-09-22 10:49:54 -0400

    first commit

--- lab_2/lab2-starter-code/project/collect_color_sensor_data.py
+++ lab_2/lab2-starter-code/project/collect_color_sensor_data.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
-
-"""
-This test is used to collect data from the color sensor.
-It must be run on the robot.
-"""
-
+# """ This test is used to collect data from the color sensor. It must be run on the robot. """
 # Add your imports here, if any
+
 from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor
-
-
+DELAY_sec = 0.01
 COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 
 # complete this based on your hardware setup
-_ = EV3ColorSensor(...)
-_ = TouchSensor(...)
+Color_Sensor = EV3ColorSensor(1)
+Touch_sensor = TouchSensor(2)
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
 
-
 def collect_color_sensor_data():
     "Collect color sensor data."
-    ...
+    try:
+        output_file = open(COLOR_SENSOR_DATA_FILE, "w")
+        while not TOUCH_SENSOR.is_pressed():
+            pass  # do nothing while waiting for first button press #
+        print("Touch sensor pressed")
+        sleep(1)
+        print("Starting to collect Color distance samples")
 
+        while not TOUCH_SENSOR.is_pressed():
+            color_data = EV3ColorSensor.get_value()  # Float value in centimeters 0, capped to 255 cm
 
-if __name__ == "__main__":
-    collect_color_sensor_data()
+            if color_data is not None:  # If None is given, then data collection failed that time
+                print(color_data)
+                output_file.write(f"{color_data}\n")
+            sleep(DELAY_SEC)
+
+    except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
+        pass
+    finally:
+        print("Done collecting US distance samples")
+        output_file.close()
+        reset_brick()  # Turn off everything on the brick's hardware, and reset it exit()
+
+    if __name__ == "__main__": collect_color_sensor_data()
